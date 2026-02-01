a2355f48ef9d159cf3f38b4f33027e3246add17d
gabriel-S-rab <serguei.roos@laposte.net>
2026-02-01 22:48:47 +0100

    ajout modif front  page index

--- public/index.php
+++ public/index.php
@@ -43,11 +43,23 @@
  </header>
     <main>
         <div class="container-fluid vw-100 vh-100">
-          <div class="container-fluid px-0 w-100 d-flex justify-content-center align-items-center" style="height: 400px; overflow: hidden;">
+          
+        <div class="container-fluid px-0 w-100 d-flex justify-content-center align-items-center" style="height: 400px; overflow: hidden;">
             <h2 style="position: absolute; color: white;">Bienvenue</h2>
-
-          <img src="../img/img1.jpg" class="img-fluid  w-100 h-100" style="object-fit: cover;" alt="...">
+            <img src="../img/img1.jpg" class="img-fluid  w-100 h-100" style="object-fit: cover;" alt="...">
           </div>
+         
+          <div class="container" style="height: 10vh"></div>  <!--div fantome -->
+
+          <div class="container-fluid d-flex justify-content-center align-items-center mt-5">
+            <div class="border   rounded p-3 col-4" style="height: 20vh; outline: 3px solid rgba(0,0,0,0.08);"><p class="flex-grow-1">Bienvenue au restaurant <strong>Le Gourmet Épicurien</strong> ! Ici, chaque plat est une création unique, préparée avec des ingrédients frais et locaux. Notre équipe met tout son savoir-faire pour vous offrir une expérience culinaire inoubliable, dans un cadre chaleureux et convivial.</p></div>
+            <div class="col-2" style="height: 20vh"></div>
+            <div class="border  rounded p-3  col-4"  style="height: 20vh; outline: 3px solid rgba(0,0,0,0.08)"><p class="flex-grow-1"> Nous vous invitons à découvrir notre carte variée, des entrées délicates aux desserts exquis, en passant par des plats principaux savoureux. Que vous veniez pour un dîner romantique, un repas en famille ou un événement spécial, <strong>Le Gourmet Épicurien</strong> saura vous séduire par sa qualité et son ambiance unique.</p></div>
+          </div>
+
+          <div class="container-fluid"> 
+          </div>
+
         </div>
  
  
