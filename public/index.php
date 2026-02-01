7d100b39189aa0833874f6ccca235d80c647e576
gabriel-S-rab <serguei.roos@laposte.net>
2026-02-01 21:02:38 +0100

    init

---
+++ public/index.php
@@ -0,0 +1,54 @@
+<!--page d'acceuil --> 
+<!DOCTYPE html> 
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Vite&Gourmand</title>
+        <link rel="stylesheet" href="css.css">
+        <script src="" defer></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
+    </head>
+<body>
+    <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #2E2E2E;" data-bs-theme="light">
+  <div class="container-fluid">
+<img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+    <a class="navbar-brand" href="#">Navbar</a>
+         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Accueil</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/galerie">Galerie</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">La carte</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Les réservations</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Mon compte</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Connexion/Deconnexion</a>
+                  </li>
+                </ul>
+         </div>
+    </nav>
+ </header>
+    <main>
+        <div class="container">
+
+        </div>
+ 
+    </main>
+    <footer>
+        <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <!-- Navbar content -->
+</nav>
+    </footer>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# vite&gourmand 
+
+# installation
